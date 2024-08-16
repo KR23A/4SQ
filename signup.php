f1ae5b80e8c3eb1b43e184248c5117b2519acd6b
Kushal Ranpura <92745978+KR23A@users.noreply.github.com>
2024-08-16 12:35:23 -0400

    Update and rename signup.html to signup.php

--- signup.php
+++ signup.php
@@ -4,7 +4,7 @@
     <meta charset = "UTF-8">
     <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
     <meta http-equiv = "X-UA-Compatible" content = "ie=edge">
-    <title>Foursquare Wholesale</title>
+    <title>Foursquare Distributors</title>
     <link rel = "stylesheet" href = "loginstyle.css">
     <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
