e3046c99bef4cd2ecdec49a1d98583cf71fe1154
Kushal Ranpura <92745978+KR23A@users.noreply.github.com>
2024-08-26 17:42:01 +0000

    js

--- javascript/pass-show-hide.js
+++ javascript/pass-show-hide.js
@@ -1,11 +1,11 @@
 const pswrdField = document.querySelector(".form .field input[type='password']");
 let toggleBtn = document.querySelector(".form .field i");
 
-toggleBtn.addEventListener (click, () => {
-    if(pswrdField.type == "password"){
+toggleBtn.addEventListener("click", () => {
+    if (pswrdField.type === "password") {
         pswrdField.type = "text";
         toggleBtn.classList.add("active");
-    }else{
+    } else {
         pswrdField.type = "password";
         toggleBtn.classList.remove("active");
     }
